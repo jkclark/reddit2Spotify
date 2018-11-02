202a4a9e85c5294abec0c61ca4ace290b77c1bda
Josh Clark <jclarktennis@gmail.com>
2018-11-02 00:18:35 -0400

    Added more unit tests.

--- test_spotify.py
+++ test_spotify.py
@@ -6,22 +6,34 @@ import spotify
 class SpotifyTestCase(unittest.TestCase):
     """Tests for spotify.py"""
 
-    def test_create_playlist(self):
-        """Agent can succesfully create playlists."""
+    def setUp(self):
         # patch __init__ to return None (avoids config, prompt for token, etc.)
         with mock.patch.object(spotify.SpotifyAgent,
                                "__init__",
                                lambda self, name, scope, config: None):
-            sp = spotify.SpotifyAgent("test_user", "test_scope", "test_file")
-            sp.username = "test_username"
+            self.sp = spotify.SpotifyAgent("test_user",
+                                           "test_scope",
+                                           "test_file")
+            self.sp.username = "test_username"
 
-            mock_agent = mock.MagicMock()
-            mock_playlist = mock.MagicMock()
-            sp.agent = mock_agent
-            sp.agent.user_playlist_create = mock_playlist
+    def test_create_playlist(self):
+        """Agent can create playlists."""
+        mock_agent = mock.MagicMock()
+        mock_playlist_snapshot = mock.MagicMock()
+        self.sp.agent = mock_agent
+        self.sp.agent.user_playlist_create = mock_playlist_snapshot
 
-            sp.create_playlist("test_pl_name", "test_pl_desc")
-            assert(mock_playlist.call_count > 0)
+        self.sp.create_playlist("test_pl_name", "test_pl_desc")
+        assert(mock_playlist_snapshot.call_count > 0)
+
+    def test_add_songs_to_playlist(self):
+        """Agent can add songs to a playlist."""
+        mock_agent = mock.MagicMock()
+        mock_playlist_snapshot = mock.MagicMock()
+        self.sp.agent = mock_agent
+        self.sp.agent.user_playlist_add_tracks = mock_playlist_snapshot
+
+        self.sp.add_songs_to_playlist(["test_track_id_1"], "test_pl_id")
 
 
 if __name__ == "__main__":
